0ac137e69090047790f52468b78a9199baef0ced
miyaichi <burnedsoba@gmail.com>
2023-01-15 00:35:42 +0900

    add tasksのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/new'
-  get 'tasks/edit'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'tasks#index'
+  resources :tasks
 end
