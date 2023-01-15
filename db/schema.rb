f8a7a530d5ecb0a40f9ae3a7e46dbc3af5852c6c
miyaichi <burnedsoba@gmail.com>
2023-01-15 22:47:56 +0900

    add TaskテーブルのnameカラムにNOT NULL制約&30文字制限を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_14_152508) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_15_134450) do
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
