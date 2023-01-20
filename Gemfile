0430de0f8df33a438b940e8ac5283bb8a55b4d47
miyaichi <burnedsoba@gmail.com>
2023-01-21 00:33:45 +0900

    add URLをリンクとして表示(rinku)

--- Gemfile
+++ Gemfile
@@ -73,3 +73,4 @@ end
 
 gem 'bootstrap', '~> 5.2.0'
 gem 'rails-i18n'
+gem 'rinku'
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,6 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
 
   belongs_to :user
+
+  scope :recent, -> { order(created_at: :desc) }
 end
