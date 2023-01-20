946aedc2ab1fc806b73d598ded76b2b1ea10d2a6
miyaichi <burnedsoba@gmail.com>
2023-01-20 23:37:45 +0900

    add tasksテーブルにuser_idカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_17_132422) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_20_143441) do
   create_table "tasks", force: :cascade do |t|
     t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
