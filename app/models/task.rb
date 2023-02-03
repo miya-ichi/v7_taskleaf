b4d20b30812cab705aadca50e81d8e3aeeed97e3
miyaichi <burnedsoba@gmail.com>
2023-02-04 00:07:22 +0900

    add CSV出力機能を実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -14,4 +14,17 @@ class Task < ApplicationRecord
   def self.ransackable_associations(auth_object = nil)
     []
   end
+
+  def self.csv_attributes
+    ['name', 'description', 'created_at', 'updated_at']
+  end
+
+  def self.generate_csv
+    CSV.generate(headers: true) do |csv|
+      csv << csv_attributes
+      all.each do |task|
+        csv << csv_attributes.map{ |attr| task.send(attr) }
+      end
+    end
+  end
 end
