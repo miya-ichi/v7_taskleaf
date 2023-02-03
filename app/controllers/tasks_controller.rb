1b4e47d8d4257af7b13080b2bbe394121c32b09e
miyaichi <burnedsoba@gmail.com>
2023-02-03 23:25:30 +0900

    fix tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -32,7 +32,7 @@ class TasksController < ApplicationController
     if @task.update(task_params)
       redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
